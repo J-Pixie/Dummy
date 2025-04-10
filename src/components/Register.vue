7494c96fe50e8817c61ff020358a580dd48df163
J-Pixie <joyalgeorege23@gmail.com>
2025-04-10 17:57:56 +0530

    Update Register.vue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -8,7 +8,7 @@
       <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1
           class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          Create an account
+          Create Your Account
         </h1>
         <form class="space-y-4 md:space-y-6" @submit.prevent="onSubmit">
           <div>
@@ -31,7 +31,7 @@
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" v-model="email" name="email" id="email"
               class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="name@company.com">
+              placeholder="name@company.in">
               <ErrorMessage name="email" class="text-yellow-300 text-sm" />
           </div>
           <div>
